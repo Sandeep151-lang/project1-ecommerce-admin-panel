1ef64685134bbbe79f4da0d25855fc25aaf0abdb
Sandeep <sandeepnandanwar92@gmail.com>
2021-12-27 20:30:25 +0530

    make it better

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var cors = require('cors');
 
 
 
-var indexRouter = require('./routes/index');
+// var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productRouter = require('./routes/createProduct');
 var admin = require('./routes/admin')
@@ -39,7 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/', usersRouter);
 app.use('/', productRouter);
 app.use('/', admin);
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var router = express.Router();
-var mongoose = require('mongoose');
+// var express = require('express');
+// var router = express.Router();
+// var mongoose = require('mongoose');
 
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// /* GET home page. */
+// router.get('/', function (req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 
 
-module.exports = router;
+// module.exports = router;
 
